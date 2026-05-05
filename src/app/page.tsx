6d4bd57b321c3f9a734d0fdd4272b19f5d7cf654
Hanna Johansson <hannajohansson16@hotmail.com>
2026-05-05 19:05:02 +0200

    Add Button component and instructions modal
    Introduce a reusable Button component with start examples variants, supports Link or button.
    Add a client-side InstructionsModal that uses the Button and handles open/close with click propagation.
    Update globals.css with new color variables.
    Add Button with Link to /game and include the InstructionsModal at startpage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
+import Button from './components/Button'
+import InstructionsModal from './components/InstructionsModal'
 
 type User = {
   id: string
@@ -24,17 +26,23 @@ export default async function Page() {
     return <div>Ingen data hittad. Checka Row Level Security (RLS) inställningarna!</div>
   }
 
-  // return (
-  //   <section className='h-svh flex flex-col justify-center items-center gap-2'>
+  return (
+    <section className='h-svh flex flex-col justify-center items-center gap-2'>
 
-  //     <h2 className='text-4xl'>Välkommen</h2>
+      <h2 className='text-4xl'>Välkommen</h2>
 
-  //     <ul>
-  //       {users?.map((user: User) => (
-  //         <li key={user.id}>{user.name}</li>
-  //       ))}
-  //     </ul>
+      <ul>
+        {users?.map((user: User) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
 
-  //   </section>
-  // )
+      <Button href="/game" className="uppercase">Go to Game Page</Button>
+      <Button variant='secondary' className="uppercase">Start Game secondary</Button>
+
+      <InstructionsModal/>
+      
+
+    </section>
+  )
 }
