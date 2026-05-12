1206177be9277a5556936a35d76aabe699e2de7c
mariatedeman <m.tedeman@hotmail.com>
2026-05-12 11:39:41 +0200

    TextInput component implemented

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import TextInput from "../components/TextInput";
+
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <h1>Home</h1>
+            <TextInput>
+                Test
+            </TextInput>
+            <TextInput>
+                Test
+            </TextInput>
+        </>
     )
 }
