8b6f61e13893623194c8f15960f2ed7b9d2b5a56
mariatedeman <m.tedeman@hotmail.com>
2026-05-13 09:19:55 +0200

    Layout width updated

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,10 @@
 export function Layout({ children } ) {
     return (
-       <main className="flex flex-col gap-2 m-8">
+       <main className="
+            flex flex-col gap-2
+            mx-auto m-8 
+            max-w-[90vw] sm:max-w-screen-sm
+        ">
             { children }
        </main>
     )
