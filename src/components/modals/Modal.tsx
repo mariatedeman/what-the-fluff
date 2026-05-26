b7da86046fcb9a1b8c6fdc93180617663ef53b72
mariatedeman <m.tedeman@hotmail.com>
2026-05-26 17:14:07 +0200

    Updated import

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import type { ModalProps } from "../models/Types";
+import type { ModalProps } from "../../models/Types";
 
 export function Modal({
   children,
