74210e74170611a4b74dca97fd25a24e5fa0c3ef
mariatedeman <m.tedeman@hotmail.com>
2026-05-26 09:28:56 +0200

    Added unused className variable in Score filter

--- src/components/scoreboard/ScoreFilter.tsx
+++ src/components/scoreboard/ScoreFilter.tsx
@@ -11,14 +11,13 @@ export function ScoreFilter({
 }: InfoPlateProps): ReactNode {
   return (
     <section className="relative w-full">
-        <div
-          className="
-                    relative z-10 w-full h-full pb-6 pt-4
+      <div
+        className={`relative z-10 w-full h-full pb-6 pt-4
                     flex items-center justify-between gap-2
-                    font-h text-white"
-        >
-          {children}
-        </div>
+                    font-h text-white ${className}`}
+      >
+        {children}
+      </div>
     </section>
   );
 }
